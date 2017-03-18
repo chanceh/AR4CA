e552e5c7f43886f90f4bf78af561e705e2c7cb06
Morior <chanceheath@gmail.com>
2017-03-17 21:23:45 -0500

    Initial Check-In

---
+++ Assets/Scripts/buttonManagerTest.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Class for Main Menu
+public class changeSceneMain : MonoBehaviour
+{
+    public Canvas mainMenu;
+    public Button designMenu;
+    public Button constructionMenu;
+
+    // Variable for loading design menu
+    public void designMenu(string sceneToDesign)
+    {
+        Application.LoadLevel(sceneToDesign);
+    }
+
+    // Variable for loading contruction menu
+    public void constructionMenu(string sceneToConstruction)
+    {
+        Application.LoadLevel(sceneToConstruction);
+    }
+
+    // Variable for exiting application
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
+
+// Class for construction environment
+public class changeScene : MonoBehaviour
+{
+    public Canvas constructionMenu;
+    public Button infoTag;
+    public Button ruler;
+    public Button grid;
+    public Button camera;
+    public Button displayType;
+    public Button opaque;
+    public Button wireframe;
+
+    public void ribbonMenu()
+    {
+        // Initialize buttons for construction environment
+        constructionMenu = constructionMenu.GetComponent<Canvas>();
+        infoTag = infoTag.GetComponent<Button>();
+        ruler = ruler.GetComponent<Button>();
+        grid = grid.GetComponent<Button>();
+        camera = camera.Getcomponent<Button>();
+        displayType = displayType.GetComponent<Button>();
+
+        // Initialize buttons for display type
+        opaque = opaque.GetComponent<Button>();
+        wireframe = wireframe.GetComponent<Button>();
+    }
+
+    public void constructionRibbonPress()
+    {
+        infoTag.enabled = true;
+        ruler.enabled = true;
+        grid.enabled = true;
+        camera.enabled = true;
+        displayType.enabled = displayTypePress;
+    }
+
+    public void constructionRibbonNoPress()
+    {
+        infoTag.enabled = false;
+        ruler.enabled = false;
+        grid.enabled = false;
+        camera.enabled = false;
+    }
+
+    // Vairable for loading display type menu
+    public void displayTypePress()
+    {
+        opaque.enabled = true;
+        wireframe.enabled = true;
+    }
+
+    // Vairable for loading component menu
+    public void componentMenu(string sceneToComponents)
+    {
+        Application.Loadlevel(sceneToComponents);
+    }
+}
---
+++ Assets/Scripts/changeScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeScene : MonoBehaviour
+{
+    public Canvas mainMenu;
+    public Button designMenu;
+    public Button constructionMenu;
+
+	// Use this for initialization
+	public void changeToScene(string sceneToChangeTo)
+    {
+        Application.LoadLevel(sceneToChangeTo);
+	}
+}
